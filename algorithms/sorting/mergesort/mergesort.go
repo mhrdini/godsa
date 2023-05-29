dd5e9f96c6170ef52c264d38c9981bc972922192
Dini Putri <mahardini.rizky@gmail.com>
2023-05-29 13:06:30 +1000

    mergesort and quicksort

---
+++ algorithms/sorting/mergesort/mergesort.go
@@ -0,0 +1,66 @@
+package mergesort
+
+import (
+	"golang.org/x/exp/constraints"
+)
+
+func Sort[T constraints.Ordered](s []T, ascending bool) {
+	mergeSort(s, 0, len(s)-1, ascending)
+}
+
+func mergeSort[T constraints.Ordered](s []T, p, r int, ascending bool) {
+	if p >= r {
+		return
+	}
+	q := (p + r) / 2
+	mergeSort(s, p, q, ascending)
+	mergeSort(s, q+1, r, ascending)
+	merge(s, p, q, r, ascending)
+}
+
+func merge[T constraints.Ordered](s []T, p, q, r int, ascending bool) {
+	llen := q - p + 1 // length of s[p:q+1]
+	rlen := r - q     // length of s[q+1:r+1]
+
+	L := append([]T{}, s[p:q+1]...)
+	R := append([]T{}, s[q+1:r+1]...)
+
+	i := 0 // smallest remaining element in L
+	j := 0 // smallest remaining element in R
+	k := p // location in s to fill
+
+	for i < llen && j < rlen {
+		switch ascending {
+		case true:
+			if L[i] <= R[j] {
+				s[k] = L[i]
+				i++
+			} else {
+				s[k] = R[j]
+				j++
+			}
+		default:
+			if L[i] >= R[j] {
+				s[k] = L[i]
+				i++
+			} else {
+				s[k] = R[j]
+				j++
+			}
+		}
+		k++
+	}
+
+	// after having gone through either L or R entirely,
+	// copy remainder of the other to the end of s[p:r]
+	for i < llen {
+		s[k] = L[i]
+		i++
+		k++
+	}
+	for j < rlen {
+		s[k] = R[j]
+		j++
+		k++
+	}
+}
---
+++ algorithms/sorting/quicksort/quicksort.go
@@ -0,0 +1,39 @@
+package quicksort
+
+import (
+	"golang.org/x/exp/constraints"
+)
+
+func Sort[T constraints.Ordered](s []T, ascending bool) {
+	quickSort(s, 0, len(s)-1, ascending)
+}
+
+func quickSort[T constraints.Ordered](s []T, p, r int, ascending bool) {
+	if p < r {
+		q := partition(s, p, r, ascending)
+		quickSort(s, p, q-1, ascending)
+		quickSort(s, q+1, r, ascending)
+	}
+}
+
+func partition[T constraints.Ordered](s []T, p, r int, ascending bool) int {
+	x := s[r]
+	i := p - 1
+	for j := p; j < r; j++ {
+		switch ascending {
+		case true:
+			if s[j] <= x {
+				i++
+				s[i], s[j] = s[j], s[i]
+			}
+		default:
+			if s[j] >= x {
+				i++
+				s[i], s[j] = s[j], s[i]
+			}
+		}
+
+	}
+	s[i+1], s[r] = s[r], s[i+1]
+	return i + 1
+}
