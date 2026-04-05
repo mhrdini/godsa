a00380447e6067fe8d046d4360491c9839033596
Dini Putri <mahardini.rizky@gmail.com>
2026-04-05 20:07:20 +0900

    add pair sum unsorted hash maps problem

---
+++ problems/bbgci/2_hashmapsandsets/hashmaps/pairsumunsorted.go
@@ -0,0 +1,39 @@
+package hashmaps
+
+/**
+ * Problem:
+ * Given an array of integers, return the indexes of any two numbers that
+ * add up to a target. The order of the indexes in the result doesn't matter.
+ * If no pair is found, return an empty array.
+ *
+ * Constraints:
+ * - The same index cannot be used twice in the result.
+ */
+
+/**
+ * Solution -> O(n)
+ *	- two passes: O(n + n) = O(n)
+ *	- first construct a map of each value to its index
+ *	- then for each value:
+ *		- calculate its complement, i.e. the other value needed to get the sum
+ *		- if the complement exists in the map i.e. in the array, then
+ *			return the index of the value and the index of the complement
+ */
+
+func PairSumUnsorted(nums []int, target int) []int {
+
+	complement_idx := map[int]int{}
+
+	for i, v := range nums {
+		complement_idx[v] = i
+	}
+
+	for i, v := range nums {
+		complement := target - v
+		idx, ok := complement_idx[complement]
+		if ok {
+			return []int{i, idx}
+		}
+	}
+	return []int{}
+}
