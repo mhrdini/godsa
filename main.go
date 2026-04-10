5c15cc3d7be1287d97a3a205e19393dfaf2b25db
Dini Putri <mahardini.rizky@gmail.com>
2026-04-10 14:49:12 +0900

    cleanup main.go

--- main.go
+++ main.go
@@ -1,11 +1,4 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/mhrdini/godsa/problems/bbgci/5_slidingwindows/dynamicslidingwindow"
-)
-
 func main() {
-	fmt.Println(dynamicslidingwindow.LongestUniformSubstringAfterReplacements("aabcdcca", 2))
 }
